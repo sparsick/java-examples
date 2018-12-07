46b1d08dc6c427ecf041a992eb16e1e330f98478
sparsick <sparsick@web.de>
2018-12-07 08:41:40 +0100

    refactoring

--- src/main/java/com/github/sparsick/java/examples/java8/a06/stream/working/FindingAndMatching.java
+++ src/main/java/com/github/sparsick/java/examples/java8/a06/stream/working/FindingAndMatching.java
@@ -1,32 +1,34 @@
 package com.github.sparsick.java.examples.java8.a06.stream.working;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 class FindingAndMatching {
 
-    private final Stream<Car> cars = Stream.of(new Car("yellow", 80),
+    private final List<Car> cars = Arrays.asList(new Car("yellow", 80),
             new Car("blue", 100),
             new Car("green", 120),
             new Car("red", 200));
 
     boolean anyMatch() {
-        return cars.anyMatch(car -> car.getHorsePower() < 150);
+        return cars.stream().anyMatch(car -> car.getHorsePower() < 150);
     }
 
     boolean allMatch() {
-        return cars.allMatch(car -> car.getHorsePower() > 50);
+        return cars.stream().allMatch(car -> car.getHorsePower() > 50);
     }
 
     boolean noneMatch() {
-        return cars.noneMatch(car -> car.getHorsePower() > 300);
+        return cars.stream().noneMatch(car -> car.getHorsePower() > 300);
     }
 
     public Optional<Car> findAny() {
-        return cars.filter(car -> car.getHorsePower() < 150).findAny();
+        return cars.stream().filter(car -> car.getHorsePower() < 150).findAny();
     }
 
     public Optional<Car> findFirst() {
-        return cars.filter(car -> car.getHorsePower() < 150).findFirst();
+        return cars.stream().filter(car -> car.getHorsePower() < 150).findFirst();
     }
 }
